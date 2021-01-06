e9a167e07f1c4e7c033944fb804c0710c2f8e4aa
Paulus Schoutsen <balloob@gmail.com>
2021-01-06 11:22:08 +0000

    Allow loading a Z-Wave driver options file
    Fixes #1
    Fixes #2

--- cli/server.ts
+++ cli/server.ts
@@ -1,14 +1,51 @@
+import mininist from "minimist";
+import { resolve } from "path";
 import { Driver } from "zwave-js";
 import express from "express";
 import { addAPItoExpress } from "../lib/express";
 
+interface Args {
+  _: Array<string>;
+  config?: string;
+}
+const expectedConfig = ["_", "config"];
+
 (async () => {
-  if (process.argv.length < 3) {
+  const args: Args = mininist(process.argv.slice(2));
+
+  const extraKeys = Object.keys(args).filter(
+    (key) => !expectedConfig.includes(key)
+  );
+  if (extraKeys.length > 0) {
+    console.error(`Got unexpected keys ${extraKeys.join(", ")}`);
+    return;
+  }
+
+  if (args._.length < 1) {
     console.error("Error: Missing path to serial port");
     return;
   }
 
-  const driver = new Driver(process.argv[2]);
+  const serialPort = args._[0];
+
+  let configPath = args.config;
+  if (configPath.substring(0, 1) !== "/") {
+    configPath = resolve(process.cwd(), configPath);
+  }
+
+  let options = undefined;
+
+  if (configPath) {
+    try {
+      options = require(configPath);
+    } catch (err) {
+      console.error(`Error: failed loading config file ${configPath}`);
+      console.error(err);
+      return;
+    }
+  }
+
+  const driver = new Driver(serialPort, options);
 
   driver.on("error", (e) => {
     console.error("Error in driver", e);
