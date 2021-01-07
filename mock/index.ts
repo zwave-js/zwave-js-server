10420060a004e3a24d1012ba0298c3f28c07f6c5
Paulus Schoutsen <balloob@gmail.com>
2021-01-07 15:06:26 +0000

    Add start_listening command

---
+++ mock/index.ts
@@ -0,0 +1,17 @@
+import type { Driver } from "zwave-js";
+import { EventEmitter } from "events";
+
+class MockController extends EventEmitter {
+  homeId = 1;
+  nodes = new Map();
+}
+
+class MockDriver extends EventEmitter {
+  public controller = new MockController();
+
+  async start() {
+    this.emit("driver ready");
+  }
+}
+
+export const createMockDriver = () => (new MockDriver() as unknown) as Driver;
