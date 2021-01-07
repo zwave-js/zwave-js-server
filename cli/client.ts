204a69b77c07ed7dde444bd2f7a7763fdc6a018d
Daniel Lando <daniel.sorridi@gmail.com>
2021-01-07 16:26:21 +0100

    chore: add linting

--- cli/client.ts
+++ cli/client.ts
@@ -1,22 +1,22 @@
-import ws from "ws";
+import ws from 'ws'
 
-const args = process.argv.slice(2).filter((val) => !val.startsWith("--"));
-const host = args.length < 1 ? "ws://localhost:3000" : args[0];
-const dump = process.argv.includes("--dump");
-const url = `${host}/zjs`;
+const args = process.argv.slice(2).filter((val) => !val.startsWith('--'))
+const host = args.length < 1 ? 'ws://localhost:3000' : args[0]
+const dump = process.argv.includes('--dump')
+const url = `${host}/zjs`
 if (!dump) {
-  console.info("Connecting to", url);
+  console.info('Connecting to', url)
 }
-const socket = new ws(url);
+const socket = new ws(url)
 
 // ws.on('open', function open() {
 //   ws.send('something');
 // });
 
-socket.on("message", (data) => {
+socket.on('message', (data) => {
   if (dump) {
-    console.log(data);
+    console.log(data)
   } else {
-    console.dir(JSON.parse(data.toString()));
+    console.dir(JSON.parse(data.toString()))
   }
-});
+})
