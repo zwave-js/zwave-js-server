41e6572231bfec703abdf3e17e68c337765aadfe
Marcel van der Veldt <m.vanderveldt@outlook.com>
2021-01-29 13:35:35 +0100

    also adjust package config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,24 @@
 module.exports = {
-  extends: ["eslint:recommended", "prettier", "prettier/@typescript-eslint"],
+  parser: "@typescript-eslint/parser", // Specifies the ESLint parser
+  parserOptions: {
+    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+    sourceType: "module", // Allows for the use of imports
+    ecmaFeatures: {
+      jsx: true // Allows for the parsing of JSX
+    }
+  },
   env: {
     es2020: true,
-    node: true,
+    node: true
   },
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: 11,
-    sourceType: "module",
-  },
-  plugins: ["@typescript-eslint"],
+  extends: [
+    "plugin:react/recommended", // Uses the recommended rules from @eslint-plugin-react
+    "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    "plugin:prettier/recommended" // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+  ],
+  rules: {
+    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+  }
 };
