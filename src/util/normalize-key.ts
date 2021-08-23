bf1a4f957ec91669b58109115f11e44fdef21f39
raman325 <7243222+raman325@users.noreply.github.com>
2021-08-23 10:18:55 -0400

    reword error

--- src/util/normalize-key.ts
+++ src/util/normalize-key.ts
@@ -6,5 +6,5 @@ export const normalizeKey = (
   if (key.length === 32) return Buffer.from(key, "hex");
   if (supportOZWFormat && key.includes("0x"))
     return Buffer.from(key.replace(/0x/g, "").replace(/, /g, ""), "hex");
-  throw new Error("Invalid key format for key " + keyName);
+  throw new Error(`Invalid key format for ${keyName} option`);
 };
