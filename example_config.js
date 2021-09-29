7e4eef335d580a147a63cabbdc64d147985ee790
Ramshield <Floriusrk@gmail.com>
2021-09-29 15:27:53 +0200

    Create example_config.js
    As per request since a full example isn't allowed (https://github.com/zwave-js/zwave-js-server/pull/391) I hope this helps someone, as this took me several days to find out.

---
+++ example_config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  logConfig: {
+    filename: "/var/log/zwave/zwave",
+    forceConsole: true,
+    logToFile: true,
+    level: "info",
+  },
+
+  storage: {
+      cacheDir: "/opt/zwave_js_server/data",
+      deviceConfigPriorityDir: "/opt/zwave_js_server/data/config",
+  },
+
+  // Generated with: "< /dev/urandom tr -dc A-F0-9 | head -c32 ;echo"
+  securityKeys: {
+      S0_Legacy: Buffer.from("<NETWORK_KEY>", "hex")
+  }
+};
