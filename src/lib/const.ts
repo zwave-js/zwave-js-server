ece2337d5e00e257ef473b1a016c0ccbedddfab6
Raman Gupta <7243222+raman325@users.noreply.github.com>
2022-06-21 03:12:37 -0400

    Bump schema

--- src/lib/const.ts
+++ src/lib/const.ts
@@ -4,6 +4,6 @@ export const version = require("../../package.json").version;
 export const minSchemaVersion = 0;
 
 // maximal/current schema version the server supports
-export const maxSchemaVersion = 18;
+export const maxSchemaVersion = 19;
 
 export const dnssdServiceType = "zwave-js-server";
