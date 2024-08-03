98ef787cf73e83643eb84495339d990a6abd9d39
Raman Gupta <7243222+raman325@users.noreply.github.com>
2024-08-03 03:40:18 -0400

    Bump zwave-js to 13.1 and schema to 38

--- src/lib/const.ts
+++ src/lib/const.ts
@@ -4,7 +4,7 @@ export const version = require("../../package.json").version;
 export const minSchemaVersion = 0;
 
 // maximal/current schema version the server supports
-export const maxSchemaVersion = 37;
+export const maxSchemaVersion = 38;
 
 export const applicationName = "zwave-js-server";
 export const dnssdServiceType = applicationName;
