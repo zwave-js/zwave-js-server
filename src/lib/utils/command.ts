1d2a01bc63dd90b38a4a53e1f85f4ba9af8471a0
Raman Gupta <7243222+raman325@users.noreply.github.com>
2024-08-21 23:11:35 -0400

    Requested comment

--- src/lib/utils/command.ts
+++ src/lib/utils/command.ts
@@ -1,9 +1,9 @@
 export enum UtilsCommand {
   parseQRCodeString = "utils.parse_qr_code_string",
   tryParseDSKFromQRCodeString = "utils.try_parse_dsk_from_qr_code_string",
-  num2hex = "utils.num2hex", // While made available in the server due to its availability within the driver, this logic should be implemented locally
-  formatId = "utils.format_id", // While made available in the server due to its availability within the driver, this logic should be implemented locally
-  buffer2hex = "utils.buffer2hex", // While made available in the server due to its availability within the driver, this logic should be implemented locally
-  getEnumMemberName = "utils.get_enum_member_name", // While made available in the server due to its availability within the driver, this logic should be implemented locally
+  num2hex = "utils.num2hex", // While made available in the server due to its availability within the driver, this functionality works best when implemented locally
+  formatId = "utils.format_id", // While made available in the server due to its availability within the driver, this functionality works best when implemented locally
+  buffer2hex = "utils.buffer2hex", // While made available in the server due to its availability within the driver, this functionality works best when implemented locally
+  getEnumMemberName = "utils.get_enum_member_name", // While made available in the server due to its availability within the driver, this functionality works best when implemented locally
   rssiToString = "utils.rssi_to_string",
 }
