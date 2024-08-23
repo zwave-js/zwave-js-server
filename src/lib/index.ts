0a8a4fbc45d9d1c241898e1ace73703819e55bfc
Raman Gupta <7243222+raman325@users.noreply.github.com>
2024-08-23 01:38:08 -0400

    Update index.ts

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,4 @@
 export * from "./command";
-
-export { ZwavejsServer } from "./server";
 export * from "./state";
-
+export { ZwavejsServer } from "./server";
 export const serverVersion: string = require("../../package.json").version;
