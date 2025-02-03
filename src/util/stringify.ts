975ac45745d2c0046aeeb6cfb4d171c7c563926a
Dominic Griesel <dominic.griesel@nabucasa.com>
2025-02-03 21:56:42 +0100

    refactor: use toJSON

--- src/util/stringify.ts
+++ src/util/stringify.ts
@@ -4,7 +4,7 @@ export function stringifyReplacer(key: string, value: any): any {
   // Ensure that Uint8Arrays are serialized as if they were Buffers
   // to keep the API backwards compatible
   if (isUint8Array(value)) {
-    return { type: "Buffer", data: Array.from(value) };
+    return Buffer.from(value).toJSON();
   }
   return value;
 }
